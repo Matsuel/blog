2d03e4209a9b8e24a08026aee1b321bc22f03523
Matsuel <matsuel0308@gmail.com>
2024-06-26 19:56:58 +0200

    add Weeks components with links

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,13 +1,13 @@
 "use client"
 
-import React, { createRef, useState } from 'react';
+import React, { createRef } from 'react';
 
 import styles from './Navbar.module.scss';
 import Image from 'next/image';
 import Logo from '@/assets/logo.png';
 import Link from 'next/link';
-import MenuSvg from '@/assets/menu.svg';
-import { LinkType, ModalProps } from '@/types/Navbar';
+import { LinkType } from '@/types/Navbar';
+import Weeks from '../Weeks';
 
 const Links: LinkType[] = [
     {
@@ -27,16 +27,8 @@ const Links: LinkType[] = [
         target: true
     },
     {
-        href: '#projects',
-        label: 'Projects',
-        icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
-            <path strokeLinecap='round' strokeLinejoin='round' d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
-        </svg>,
-        target: false
-    },
-    {
-        href: '#work',
-        label: 'Work',
+        href: 'glossary',
+        label: 'Glossary',
         icon: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
             <path strokeLinecap='round' strokeLinejoin='round' d="M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z" />
         </svg>,
@@ -45,9 +37,6 @@ const Links: LinkType[] = [
 ];
 
 const Navbar = () => {
-
-    const [activeLink, setActiveLink] = useState<number>(-1);
-    const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
     const navRef = createRef<HTMLDivElement>();
 
     if (typeof window !== 'undefined') {
@@ -73,42 +62,15 @@ const Navbar = () => {
 
             <div className={styles.Navbar_links}>
                 {Links.map((link, index) => (
-                    <Link key={link.label} href={link.href} target={link.target ? "_blank" : "_self"} className={styles.Navbar_link + " " + (activeLink === index ? styles.Navbar_linkActive : "")} onClick={() => setActiveLink(index)} >
+                    <Link key={link.label} href={link.href} target={link.target ? "_blank" : "_self"} className={styles.Navbar_link}>
                         {link.icon}
                         {link.label}
                     </Link>
                 ))}
+                <Weeks />
             </div>
-            <Image src={MenuSvg} alt="Menu" className={styles.Navbar_menu} onClick={() => setIsModalOpen(!isModalOpen)} />
-            <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} links={Links} activeLink={activeLink} setActiveLink={setActiveLink} />
         </div>
     );
 };
 
 export default Navbar;
-
-const Modal = ({
-    isOpen,
-    onClose,
-    links,
-    activeLink,
-    setActiveLink
-}: ModalProps) => {
-    return (
-        <>
-            {isOpen && (
-                <div className={styles.Modal_container}>
-                    {
-                        links.map((link, index) => (
-                            <Link key={link.label} href={link.href} className={styles.Modal_link + " " + (activeLink === index ? styles.Modal_linkActive : "")} onClick={() => { setActiveLink(index); onClose(); }}>
-                                {link.icon}
-                                {link.label}
-                            </Link>
-                        ))
-                    }
-                </div>
-            )}
-
-        </>
-    );
-}
---
+++ src/types/Weeks.ts
@@ -0,0 +1,4 @@
+export type Week = {
+    title: string;
+    href: string;
+};

194b289b4298d627bbd20482c29a13c7c9f9d99d
Matsuel <matsuel0308@gmail.com>
2024-06-27 13:34:18 +0200

    add articles and glossary pages

---
+++ src/app/articles/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Articles.module.scss'
+import Navbar from '@/components/Navbar'
+
+const page = () => {
+  return (
+    <div className={styles.container}>
+        <Navbar />
+        
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/glossary/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Glossary.module.scss'
+import Navbar from '@/components/Navbar'
+
+const page = () => {
+  return (
+    <div className={styles.container}>
+        <Navbar />
+        
+    </div>
+  )
+}
+
+export default page

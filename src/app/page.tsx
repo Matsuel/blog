bd23c32889885fd0ec765529e3197f731c9b76bf
Matsuel <matsuel0308@gmail.com>
2024-07-02 19:26:55 +0200

    add entreprise

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,10 @@
 import Navbar from "@/components/Navbar";
 import styles from "./page.module.scss";
 import Title from "@/components/Title";
+import Shinken from "@/assets/shinken.png"
+import LinkSvg from "@/assets/link.svg";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -12,6 +16,12 @@ export default function Home() {
         content="Stage 2024"
         emojiCode="1f468-200d-1f4bb"
       />
+
+      <Link className={styles.entreprise} href="https://www.shinken-enterprise.com/" target="_blank">
+        <Image src={Shinken} alt="Shinken" className={styles.shinkenLogo} />
+        <Title content="Shinken Solutions" emojiCode="" />
+        <Image src={LinkSvg} alt="Link" className={styles.linkSvg} />
+      </Link>
 
     </main>
   );

95f0669d279d69846aa47fc2d07b028a76a98208
Matsuel <matsuel0308@gmail.com>
2024-07-11 13:43:14 +0200

    add todo in title

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -17,7 +17,7 @@ const Title: React.FC<TitleProps> = ({
     ...props
 }) => {
     return (
-        //split ça en 2 composants 1 avec la div et l'emoji et l'autre avec le title en prenant props en arg
+        //split ça en 2 composants 1 avec la div et l'emoji et l'autre avec le title en prenant props en arg pour pouvoir l'utiliser comme un vraiment composant
         <div className={styles.Wrapper}>
             <h1 className={styles.Title} {...props}>
                 {Capitalize(content)}

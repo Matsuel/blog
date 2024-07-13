e321518c57d9a24ace1ad5242883734a8d18e1a8
Matsuel <matsuel0308@gmail.com>
2024-07-13 21:59:59 +0200

    change navbar buttons + edit titles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,35 +3,38 @@
 import Navbar from "@/components/Navbar";
 import styles from "./page.module.scss";
 
-import TitleComponent from "@/components/Title";
 import Shinken from "@/assets/shinken.png"
 import LinkSvg from "@/assets/link.svg";
 import Image from "next/image";
 import Link from "next/link";
 import Button from "@/components/Button";
-import ChevronRight from "@/assets/ChevronRight.svg";
+import EmojiTitle from "@/components/Title/EmojiTitle";
+import Title from "@/components/Title/Title";
 
 export default function Home() {
 
   return (
     <main className={styles.main}>
       <Navbar />
-      <TitleComponent emojiCode="1f468-200d-1f4bb">
-        Stage 2024
-      </TitleComponent>
+      <EmojiTitle emojiCode="1f468-200d-1f4bb">
+        <Title>
+          Stage 2024
+        </Title>
+      </EmojiTitle>
 
       <Link className={styles.entreprise} href="https://www.shinken-enterprise.com/" target="_blank">
         <Image src={Shinken} alt="Shinken" className={styles.shinkenLogo} />
-        {/* Supprimer ce composant et mettre EmojiTitle et Title à la place comme ça on peut appliquer les styles comme on veut */}
-        <TitleComponent style={{ fontSize: '5rem'}}>
+        <Title>
           Shinken Solutions
-        </TitleComponent>
+        </Title>
         <Image src={LinkSvg} alt="Link" className={styles.linkSvg} />
       </Link>
 
-      <Button style={{ marginTop: "100px" }}>
+      <Button style={{ marginTop: "100px" }} variant="primary">
         Télécharger le PDF
-        <Image src={ChevronRight} alt="ChevronRight" className={styles.chevronRight} />
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="3" stroke="currentColor" width="24" height="24">
+          <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+        </svg>
       </Button>
 
     </main>

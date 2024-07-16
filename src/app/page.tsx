00ce899b9fa4ec8696441f27f308eb50f633ffe6
Matsuel <matsuel0308@gmail.com>
2024-07-16 13:56:11 +0200

    edit colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,9 @@
 
 import Navbar from "@/components/Navbar";
 import styles from "./page.module.scss";
-
-import Shinken from "@/assets/shinken.png"
-import LinkSvg from "@/assets/link.svg";
-import Image from "next/image";
-import Link from "next/link";
-import Button from "@/components/Button";
 import EmojiTitle from "@/components/Title/EmojiTitle";
 import Title from "@/components/Title/Title";
+import Button from "@/components/Button";
 
 export default function Home() {
 
@@ -24,7 +19,21 @@ export default function Home() {
 
       <div className={styles.container}>
       {/* Lister les entreprises avec une image, un badge avec le côté bossé (front back), le nom de l'entreprise, le nom du stagiaire, la date de début et de fin du stage, le lien vers le site de l'entreprise, le lien vers le rapport de stage. */}
+        <Button>
+          cc
+        </Button>
 
+        <Button variant="light">
+          Light
+        </Button>
+
+        <Button variant="dark">
+          Dark
+        </Button>
+
+        <Button variant="primary">
+          Primary
+        </Button>
       </div>
     </main>
   );

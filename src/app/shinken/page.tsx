a0cd6cebe7b6943f143f4b120042e432c6abdaea
Matsuel <matsuel0308@gmail.com>
2024-07-23 13:53:41 +0200

    Delete useSearchParams

--- src/app/shinken/page.tsx
+++ src/app/shinken/page.tsx
@@ -1,20 +1,13 @@
 "use client"
 
-
-import { useSearchParams } from 'next/navigation'
-import React, { Suspense } from 'react'
+import React from 'react'
 import DefaultLayout from '@/components/DefaultLayout'
 
 const Shinken = () => {
 
-  const param = useSearchParams()
-
   return (
-    <Suspense fallback={<div>Loading</div>}>
-      <DefaultLayout>
-        {param.get('name')}
-      </DefaultLayout>
-    </Suspense>
+    <DefaultLayout>
+    </DefaultLayout>
   )
 }
 

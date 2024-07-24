88f0d64b9d1893704d59c0d0ed5848616039fb21
Matsuel <matsuel0308@gmail.com>
2024-07-24 13:41:11 +0200

    Creation of components for articles

--- src/app/shinken/page.tsx
+++ src/app/shinken/page.tsx
@@ -2,15 +2,56 @@
 
 import React from 'react'
 import DefaultLayout from '@/components/DefaultLayout'
-import { useSearchParams } from 'next/navigation'
+import styles from './Shinken.module.scss'
+import Badge from '@/components/Badge'
+import { Emoji } from 'emoji-picker-react'
+import ReactMarkdown from 'react-markdown';
+import Image from 'next/image'
+import NewProps from '@/assets/host_new_property.png'
 
 const Shinken = () => {
 
-  const params = useSearchParams()
-
   return (
     <DefaultLayout>
-      {params.get('name')}
+      <div className={styles.article}>
+
+        <div className={styles.articleBadge}>
+          <Badge variant='back'>
+            <Emoji unified={"1f4bb"} size={15} />
+            Front
+
+          </Badge>
+        </div>
+
+        <div className={styles.articleTitle}>
+          Article 1
+        </div>
+
+        <div className={styles.articleSubtitle}>
+          Article 1
+        </div>
+
+        <ReactMarkdown className={styles.articleParagraphe}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin molestie turpis sit amet nibh tristique, a varius arcu tristique. Sed aliquam arcu leo, ornare feugiat lacus ullamcorper ac. Aliquam erat volutpat. Ut viverra risus non nisl rutrum, at venenatis eros rutrum. Nunc ac lacinia lacus, a commodo lorem. Aenean non viverra urna. Suspendisse euismod luctus nisi, lobortis placerat risus hendrerit quis. Donec nibh augue, finibus vel dapibus nec, lacinia sed est. Duis ultrices vulputate bibendum. Suspendisse at mattis ante.
+        </ReactMarkdown>
+
+        <Image
+          src={NewProps}
+          className={styles.articleImage}
+          alt='cc'
+        />
+
+        {/* Arguements direction row ou column */}
+        {/* Arguements index pour liste nuérotée */}
+        <div className={styles.articleList}>
+          <div className={styles.articleListElement}>1</div>
+          <div className={styles.articleListElement}>1</div>
+          <div className={styles.articleListElement}>1</div>
+          <div className={styles.articleListElement}>1</div>
+        </div>
+
+      </div>
+
     </DefaultLayout>
   )
 }

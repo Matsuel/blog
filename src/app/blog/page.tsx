e01e4b4d791decff56ed246f917e73e8f3280f5e
Matsuel <matsuel0308@gmail.com>
2024-07-30 14:30:43 +0200

    Component Home -> Create Home Page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import styles from "./page.module.scss";
+import EmojiTitle from "@/components/Title/EmojiTitle";
+import Entreprise from "@/components/Entreprise";
+import { Entreprises } from "@/datas/Entreprises";
+import { Articles } from "@/datas/Articles";
+import ArticleHome from "@/components/ArticleHome";
+import DefaultLayout from "@/components/DefaultLayout";
+
+export default function Blog() {
+
+  return (
+    <DefaultLayout emojiCode="1f468-200d-1f4bb" title="Blog Stages">
+      <div className={styles.dataContainer}>
+
+        <EmojiTitle emojiCode="1f3e2" size={30}>
+          <h2 className={styles.containerTitle}>
+            Entreprises
+          </h2>
+        </EmojiTitle>
+        {Entreprises.map((stage) => (
+          <Entreprise {...stage} key={stage.name} />
+        ))}
+      </div>
+
+
+      <div className={styles.dataContainer}>
+        <EmojiTitle emojiCode="1f4da" size={30}>
+          <h2 className={styles.containerTitle}>
+            Articles
+          </h2>
+        </EmojiTitle>
+        {Articles.map((article) => (
+          <ArticleHome {...article} key={article.description} />
+        ))}
+      </div>
+
+    </DefaultLayout >
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,43 +1,36 @@
 "use client"
 
-import styles from "./page.module.scss";
-import EmojiTitle from "@/components/Title/EmojiTitle";
-import Entreprise from "@/components/Entreprise";
-import { Entreprises } from "@/datas/Entreprises";
-import { Articles } from "@/datas/Articles";
-import ArticleHome from "@/components/ArticleHome";
-import DefaultLayout from "@/components/DefaultLayout";
-import EmojiPicker from "emoji-picker-react";
+import DefaultLayout from '@/components/DefaultLayout'
+import React from 'react'
+import styles from './Page.module.scss'
+import { HomeDatas } from '@/datas/Home'
+
+const Home = () => {
+    return (
+        <DefaultLayout title='Matheo' emojiCode="1f3e1">
+            Qui suis-je?
+            CV
+            Parcours
+            Lien vers portfolio
+
+            Lien grafiakrt https://www.youtube.com/watch?v=YNbPMm08jcw
 
 
-export default function Home() {
-
-  return (
-    <DefaultLayout emojiCode="1f468-200d-1f4bb" title="Blog Stages">
-      <div className={styles.dataContainer}>
-
-        <EmojiTitle emojiCode="1f3e2" size={30}>
-          <h2 className={styles.containerTitle}>
-            Entreprises
-          </h2>
-        </EmojiTitle>
-        {Entreprises.map((stage) => (
-          <Entreprise {...stage} key={stage.name} />
-        ))}
-      </div>
+            <div className={styles.presentation}>
+                {HomeDatas['paragraphe'].map((paragraphe)=>(
+                    <p className={styles.paragraphe}>
+                        {paragraphe}
+                    </p>
+                ))}
+            </div>
 
 
-      <div className={styles.dataContainer}>
-        <EmojiTitle emojiCode="1f4da" size={30}>
-          <h2 className={styles.containerTitle}>
-            Articles
-          </h2>
-        </EmojiTitle>
-        {Articles.map((article) => (
-          <ArticleHome {...article} key={article.description} />
-        ))}
-      </div>
 
-    </DefaultLayout >
-  )
+
+
+
+        </DefaultLayout>
+    )
 }
+
+export default Home

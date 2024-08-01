95142fbd8be5b3d00d73ca850705afacdae82b5f
Matsuel <matsuel0308@gmail.com>
2024-08-01 14:19:05 +0200

    Component Home -> Add Parcours

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import DefaultLayout from '@/components/DefaultLayout'
-import React from 'react'
+import React, { ReactNode } from 'react'
 import styles from './page.module.scss'
 import { HomeDatas } from '@/datas/Home'
 import Button from '@/components/Button'
@@ -26,45 +26,60 @@ const Home = () => {
                 ))}
             </div>
 
-            <div className={styles.parcours}>
-                {HomeDatas.parcours.map((cc)=>(
-                    <div>
-                        {cc.city}
+            <div className={styles.about}>
+
+                <div className={styles.homeLinks}>
+                    <Title>
+                        Liens
+                    </Title>
+
+                    <div className={styles.homeButtons}>
+                        <Link href='/CV.pdf' target='_blank'>
+                            <Button variant='dark'>
+                                CV
+                            </Button>
+                        </Link>
+                        <Link href={HomeDatas.portfolio} target='_blank'>
+                            <Button variant='primary'>
+                                Portfolio (anglais uniquement)
+                            </Button>
+                        </Link>
+                        <Link href={HomeDatas.linkedin} target='_blank'>
+                            <Button variant='primary'>
+                                Linkedin
+                            </Button>
+                        </Link>
+                        <Link href='/blog'>
+                            <Button variant='primary'>
+                                Visiter le blog
+                            </Button>
+                        </Link>
                     </div>
-                ))}
-            </div>
 
-
-            <div className={styles.homeLinks}>
-                <Title>
-                    Liens
-                </Title>
-
-                <div className={styles.homeButtons}>
-                <Link href='/CV.pdf' target='_blank'>
-                        <Button variant='dark'>
-                            CV
-                        </Button>
-                    </Link>
-                    <Link href={HomeDatas.portfolio} target='_blank'>
-                        <Button variant='primary'>
-                            Portfolio (anglais uniquement)
-                        </Button>
-                    </Link>
-                    <Link href={HomeDatas.linkedin} target='_blank'>
-                        <Button variant='primary'>
-                            Linkedin
-                        </Button>
-                    </Link>
                 </div>
 
-                <Link href='/blog'>
-                    <Button variant='primary'>
-                        Visiter le blog
-                    </Button>
-                </Link>
-            </div>
+                <div className={styles.parcours}>
+                    {HomeDatas.parcours.map((parcours) => (
+                        <div className={styles.parcoursRow} key={parcours.name}>
+                            <div className={styles.parcoursIcon}>
+                                {parcours.icon}
+                            </div>
+                            <div className={styles.parcoursInfos}>
+                                <div className={styles.parcoursInfosContainer}>
+                                    <h4 className={styles.parcoursName}>
+                                        {parcours.name}
+                                    </h4>
+                                    <p className={styles.parcoursDates}>
+                                        {parcours.dates}
+                                    </p>
+                                </div>
+                                {parcours.city}
+                            </div>
 
+                        </div>
+                    ))}
+                </div>
+            </div>
         </DefaultLayout>
     )
 }

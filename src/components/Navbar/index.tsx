85f0818e25b64dea81d7fcb8079716d353fb4fe5
Matsuel <matsuel0308@gmail.com>
2024-08-02 14:31:07 +0200

    Component Navbar -> Toggle Button and create useSize Hook

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,30 +1,46 @@
 "use client"
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './Navbar.module.scss';
 import Link from 'next/link';
 import Button from '../Button';
 import { Links } from '@/datas/Navbar';
+import Menu from '@/assets/menu.svg'
+import Image from 'next/image';
+import useSize from '@/hooks/useSize';
 
 const Navbar = () => {
 
+    const windowsize = useSize();
+    const [showModal, setShowModal] = useState<boolean>(false);
+
+
     return (
         <div className={styles.Navbar_container}>
-            <Link className={styles.Navbar_title} href="/">
-                Matheo Lang
-            </Link>
 
-            <div className={styles.Navbar_links}>
-                {Links.map((link) => (
-                    <Link key={link.label} href={link.href} target={link.target ? '_blank' : ''}>
-                        <Button key={link.label} variant={link.target ? "dark" : "primary"}>
-                            {link.icon}
-                            {link.label}
-                        </Button>
+            <button className={styles.Navbar_Menu} onClick={() => setShowModal(!showModal)}>
+                <Image src={Menu} alt='menu' />
+            </button>
+            
+            {(windowsize[0] > 512 || showModal) &&
+                <>
+                    <Link className={styles.Navbar_title} href="/">
+                        Matheo Lang
                     </Link>
-                ))}
-            </div>
+
+                    <div className={styles.Navbar_links}>
+                        {Links.map((link) => (
+                            <Link key={link.label} href={link.href} target={link.target ? '_blank' : ''}>
+                                <Button key={link.label} variant={link.target ? "dark" : "primary"}>
+                                    {link.icon}
+                                    {link.label}
+                                </Button>
+                            </Link>
+                        ))}
+                    </div>
+                </>
+            }
         </div>
     );
 };

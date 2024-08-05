44596c5fafda50a38cff1bc5d3c2628628a77ac6
Matsuel <matsuel0308@gmail.com>
2024-08-05 13:32:37 +0200

    Entreprise Shinken -> Escape apos

--- src/components/Articles/Shinken/index.tsx
+++ src/components/Articles/Shinken/index.tsx
@@ -12,30 +12,30 @@ const ShinkenArticle = ({
     return (
         <ArticleLayout title={title}>
             Shinken Solutions est une entreprise dynamique spécialisée dans la supervision informatique. 
-            Fondée en 2009 par Jean Gabès, cette entreprise a rapidement su s'imposer comme un acteur incontournable dans le domaine de la surveillance des infrastructures IT. 
+            Fondée en 2009 par Jean Gabès, cette entreprise a rapidement su s&aposimposer comme un acteur incontournable dans le domaine de la surveillance des infrastructures IT. 
             Située à Artigues-près-Bordeaux entre la sortie 25 et 26 de la rocade bordelaise, Shinken Solutions bénéficie d’un emplacement stratégique dans le Sud-Ouest de la France.
 
-            L'objectif principal de Shinken était de créer une solution de monitoring open source qui éait basée sur des technologies récentes (Python et Javascript) pour l'époque et simple de développement (comparé à Nagios qui est fait en C le langage star de l'époque).
-            Shinken se concentre sur la fourniture d'une solution de monitoring dite Scalable (qui est capable de fonctionner dans une petite entreprise mais également dans une grosse entreprise disposant de 15000 serveurs) grace à son architecture globale sous forme de démons.
-            Chaque démons dispose d'un rôle qui lui es attribué, les différents démons sont les suivants : 
+            L&aposobjectif principal de Shinken était de créer une solution de monitoring open source qui éait basée sur des technologies récentes (Python et Javascript) pour l&aposépoque et simple de développement (comparé à Nagios qui est fait en C le langage star de l&aposépoque).
+            Shinken se concentre sur la fourniture d&aposune solution de monitoring dite Scalable (qui est capable de fonctionner dans une petite entreprise mais également dans une grosse entreprise disposant de 15000 serveurs) grace à son architecture globale sous forme de démons.
+            Chaque démons dispose d&aposun rôle qui lui es attribué, les différents démons sont les suivants : 
             - Synchronizer  (gère interface de configuration et db)
-            - Arbiter (chef d'orchestre)
-            - Receiver (reçoit les ordres via son API et les transmet à l'arbiter)
+            - Arbiter (chef d&aposorchestre)
+            - Receiver (reçoit les ordres via son API et les transmet à l&aposarbiter)
             - Schdeuler (récupère toutes les datas et les retransmet)
-            - Puller (exécute les différents tests afin de vérifier que l'objet supervisé fonctionne correctement)
-            - Broker (gère l'interface de visualisation, il lit la db et affiche les infos)
-            - Reactionner (sert à envoyer les notifications lors d'une changement d'état, lors de la perte de résultat d'un test)
+            - Puller (exécute les différents tests afin de vérifier que l&aposobjet supervisé fonctionne correctement)
+            - Broker (gère l&aposinterface de visualisation, il lit la db et affiche les infos)
+            - Reactionner (sert à envoyer les notifications lors d&aposune changement d&aposétat, lors de la perte de résultat d&aposun test)
 
-            Cette architecure est Scalable car chaque démon peut-être dupliqué afin de décupler les performances de la solution, cela permet également de gérer plus d'objets et donc de s'adapter à l'organisation de chaque client.
-
-
-            Les journées de travail s'effectue dans un open space organisé en fonction de la spécialité de chacun (front ou back). Chacun est sur une problématique à résoudre qui sont recensées sur des tickets afin de permettre de suivre l'évolution et de résoudre les problèmes/bugs rencontrés.
-            Chaque développeur dispose d'un pc fix sous Windows, la solution est localement installée sur un machine virtuelle afin de pourvoir effectuer les différents développement sur la version adéquat.
-            Chaque machine virtuelle est accessible depuis n'importe quel poste du réseau afin que les tech leads ou autre puissent nous dépanner depuis leur place.
+            Cette architecure est Scalable car chaque démon peut-être dupliqué afin de décupler les performances de la solution, cela permet également de gérer plus d&aposobjets et donc de s&aposadapter à l&aposorganisation de chaque client.
 
 
+            Les journées de travail s&aposeffectue dans un open space organisé en fonction de la spécialité de chacun (front ou back). Chacun est sur une problématique à résoudre qui sont recensées sur des tickets afin de permettre de suivre l&aposévolution et de résoudre les problèmes/bugs rencontrés.
+            Chaque développeur dispose d&aposun pc fix sous Windows, la solution est localement installée sur un machine virtuelle afin de pourvoir effectuer les différents développement sur la version adéquat.
+            Chaque machine virtuelle est accessible depuis n&aposimporte quel poste du réseau afin que les tech leads ou autre puissent nous dépanner depuis leur place.
 
-            <h1>Icones entreprise, carte avec l'emplcament ainsi que icones des logiciels et des langages</h1>
+
+
+            <h1>Icones entreprise, carte avec l&aposemplcament ainsi que icones des logiciels et des langages</h1>
         </ArticleLayout >
     )
 }

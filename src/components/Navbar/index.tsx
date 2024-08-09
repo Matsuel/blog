6927f77fbd4a3f5d50b65a99625517585bca0b6d
Matsuel <matsuel0308@gmail.com>
2024-08-09 15:03:06 +0200

    Component Navbar -> Add ref ClickAway for close on click

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState } from 'react';
+import React, { LegacyRef, useState } from 'react';
 
 import styles from './Navbar.module.scss';
 import Link from 'next/link';
@@ -9,21 +9,25 @@ import { Links } from '@/datas/Navbar';
 import Menu from '@/assets/menu.svg'
 import Image from 'next/image';
 import useSize from '@/hooks/useSize';
+import { useClickAway } from '@uidotdev/usehooks';
 
 const Navbar = () => {
 
     const windowsize = useSize();
     const [showModal, setShowModal] = useState<boolean>(false);
     const isClient = typeof window === 'object';
+    const navClickAway = useClickAway(() => {
+        setShowModal(false);
+    })
 
 
     return (
-        <div className={styles.Navbar_container}>
+        <div className={styles.Navbar_container} ref={(isClient && windowsize[0] < 512) ? navClickAway as LegacyRef<HTMLDivElement> : null}>
 
             <button className={styles.Navbar_Menu} onClick={() => setShowModal(!showModal)}>
                 <Image src={Menu} alt='menu' />
             </button>
-            
+
             {(isClient && (windowsize[0] > 512 || showModal)) &&
                 <>
                     <Link className={styles.Navbar_title} href="/">
